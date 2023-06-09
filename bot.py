1042f9556763dbe88c814337c5e1622ed520cfd5
Jade Pughe <jadepughe@gmail.com>
2023-06-09 15:17:10 +0100

    fixed typo in line 43

--- bot.py
+++ bot.py
@@ -40,7 +40,7 @@ async def on_message(message):
 	# if the message mentions the bot, then do something
 	if client.user.mentioned_in(message): 
 		if any(greeting in message.content.lower() for greeting in ['hi', 'hello', 'hey']):
-			respone = "Hi, I'm your leftovers bot! Please give me a list of your leftovers, and I will generate a recipe for you. Don't forget to mention me @eat-gpt in the message!"
+			response = "Hi, I'm your leftovers bot! Please give me a list of your leftovers, and I will generate a recipe for you. Don't forget to mention me @eat-gpt in the message!"
 			await message.channel.send(response)
 		else:
 			response = openai.ChatCompletion.create(
