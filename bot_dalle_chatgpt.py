551a2cf9164d86f9e8b8fb8c0300c05614d4ab6e
Jade Pughe <jadepughe@gmail.com>
2023-06-14 15:28:26 +0100

    combined dalle and chatgpt code

--- bot_dalle_chatgpt.py
+++ bot_dalle_chatgpt.py
@@ -1,68 +1,75 @@
+import logging
+import os
 import requests
 import time
-import os
 from PIL import Image
 import discord
 import openai
-import logging  
+
+logging.basicConfig(level=logging.INFO)
+logging.info('Worker script started')
 
 openai.api_type = "azure"
-openai.api_version = '2022-08-03-preview'
+openai.api_version = "2023-03-15-preview"
+
+# specifying our server
+GUILD = "{ardubabe's server}"
 
 # create an object that will control our discord bot
 client = discord.Client(intents=discord.Intents.default())
-
-with open("keys.txt") as f:
-    # converting our text file to a list of lines
-    lines = f.read().split('\n')
-    # openai api key
-    openai.api_key = lines[1]
-    # discord token
-    DISCORD_TOKEN = lines[7]
-    openai.api_base = lines[9]
-# close the file
-f.close()
+# env variables to be read by railway
+openai.api_key = os.environ.get("API_KEY")
+DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN")
+openai.api_base = os.environ.get("API_BASE")
 
 @client.event
 async def on_ready():
-    # print out nice statment saying our bot is online (only in command prompt)
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+    # print out a nice statement saying our bot is online (only in command prompt)
     print(f'{client.user} has connected to Discord!')
 
 @client.event
 async def on_message(message):
-    # this prevents inifinte loops of bot talking to bot
-    # if author of the message is the bot, don't do anything
-    if message.author == client.user:
+    if message.author == client.user or message.mention_everyone:
         return
-    # ignore @everyone mentions
-    if message.mention_everyone:
-        return
-    # if the message mentions the bot, then do something
-    elif client.user.mentioned_in(message): 
+    
+    if client.user.mentioned_in(message):
+        # Generate recipe using OpenAI Chat API
+        response = openai.ChatCompletion.create(
+            engine="GPT-4",
+            messages=[
+                {"role": "system", "content": "I will give you a list of leftover food. Give me a recipe I can make using only the leftovers I have provided and Hellmann's mayonnaise. Provide the dish name, don't title it dish name. Then give me the ingredients list in the format of '- Base, - Fruit and Veg, - Protein and - Magic Touch'. Base is carbohydrates, and magic touch is always Hellmann's Mayonnaise. Then give me the instructions to make the meal in simple terms. Make sure all responses are less than 1500 characters"},
+                {"role": "user", "content": message.content}
+            ]
+        )
+        recipe = response.choices[0].message.content
+
+        # Extract the dish name from the recipe response
+        dish_name = recipe.splitlines()[0]
+
+        # Generate DALL-E image using OpenAI API
         url = "{}dalle/text-to-image?api-version={}".format(openai.api_base, openai.api_version)
-        headers= { "api-key": openai.api_key, "Content-Type": "application/json" }
+        headers = {"api-key": openai.api_key, "Content-Type": "application/json"}
         body = {
-            "caption": message.content,
+            "caption": dish_name,
             "resolution": "1024x1024"
         }
         submission = requests.post(url, headers=headers, json=body)
         operation_location = submission.headers['Operation-Location']
         retry_after = submission.headers['Retry-after']
         status = ""
-        while (status != "Succeeded"):
+
+        # Wait for image generation to complete
+        while status != "Succeeded":
             time.sleep(int(retry_after))
             response = requests.get(operation_location, headers=headers)
             status = response.json()['status']
         image_url = response.json()['result']['contentUrl']
-        print(image_url)
 
-        response = openai.ChatCompletion.create(
-			engine="GPT-4",
-			messages=[
-			{"role": "system", "content": "I will give you a list of leftover food. Give me a recipe I can make using only the leftovers I have provided and Hellmann's mayonnaise. Provide the dish name, don't title it dish name. Then give me the ingredients list in the format of '- Base, - Fruit and Veg, - Protein and - Magic Touch'. Base is carbohydrates, and magic touch is always Hellmann's Mayonnaise. Then give me the instructions to make the meal in simple terms. Make sure all responses are less than 1500 characters"},
-			{"role": "user", "content": message.content}
-			]
-		)
-        await message.channel.send(image_url, response.choices[0].message.content)
+        # Send recipe and image URLs as responses to the Discord channel
+        await message.channel.send(recipe)
+        await message.channel.send(image_url)
 
 client.run(DISCORD_TOKEN)
