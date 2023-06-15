af73044c4da48b6f266f0eb185c2ccf91c0f2800
Jade Pughe <jadepughe@gmail.com>
2023-06-15 11:28:03 +0100

    changing caption to recipe_name

--- bot_dalle_chatgpt.py
+++ bot_dalle_chatgpt.py
@@ -17,37 +17,40 @@ GUILD = "{ardubabe's server}"
 
 # create an object that will control our discord bot
 client = discord.Client(intents=discord.Intents.default())
+
 # env variables to be read by Digital Ocean
 openai.api_key = os.environ.get("API_KEY")
 DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN")
 openai.api_base = os.environ.get("API_BASE")
 
-# DALL-E image generation function
-async def generate_dalle_image(caption):
-    # url = f"{openai.api_base}dalle/text-to-image?api-version={openai.api_version}"
-    url = "{}dalle/text-to-image?api-version={}".format(openai.api_base, openai.api_version)
-    headers = {"api-key": openai.api_key, "Content-Type": "application/json"}
-    body = {
-        "caption": caption,
-        #"caption": dish_name,
-        "resolution": "1024x1024"
-    }
-    submission = requests.post(url, headers=headers, json=body)
-    operation_location = submission.headers['Operation-Location']
-    print(submission.headers)
-    retry_after = submission.headers['Retry-after']
-    status = ""
-    while status != "Succeeded":
-        time.sleep(int(retry_after))
-        response = requests.get(operation_location, headers=headers)
-        print(operation_location)
-        status = response.json()['status']
-    image_url = response.json()['result']['contentUrl']
-    return image_url
-
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+# # DALL-E image generation function
+# async def generate_dalle_image(caption):
+#     # url = f"{openai.api_base}dalle/text-to-image?api-version={openai.api_version}"
+#     url = "{}dalle/text-to-image?api-version={}".format(openai.api_base, openai.api_version)
+#     headers = {"api-key": openai.api_key, "Content-Type": "application/json"}
+#     body = {
+#         "caption": caption,
+#         #"caption": dish_name,
+#         "resolution": "1024x1024"
+#     }
+#     submission = requests.post(url, headers=headers, json=body)
+#     operation_location = submission.headers['Operation-Location']
+#     print(submission.headers)
+#     retry_after = submission.headers['Retry-after']
+#     status = ""
+#     while status != "Succeeded":
+#         time.sleep(int(retry_after))
+#         response = requests.get(operation_location, headers=headers)
+#         print(operation_location)
+#         status = response.json()['status']
+#     image_url = response.json()['result']['contentUrl']
+#     return image_url
+
+
 
 @client.event
 async def on_message(message):
@@ -71,12 +74,26 @@ async def on_message(message):
                     {"role": "user", "content": message.content}
                 ]
             )
-            recipe = response.choices[0].message.content
-            recipe_lines = recipe.split('\n')
-            dish_name = recipe_lines[0]
+            recipe_name = response.choices[0].message.content.split('\n')[0]  # Extract the recipe name
 
-            await message.channel.send(recipe)
-            image_url = generate_dalle_image(dish_name)
+            # Use recipe name as prompt for DALL-E image generation
+            url = "{}dalle/text-to-image?api-version={}".format(openai.api_base, openai.api_version)
+            headers = {"api-key": os.environ.get("API_KEY"), "Content-Type": "application/json"}
+            body = {
+                "caption": recipe_name,
+                "resolution": "1024x1024"
+            }
+            submission = requests.post(url, headers=headers, json=body)
+            operation_location = submission.headers['Operation-Location']
+            retry_after = submission.headers['Retry-after']
+            status = ""
+            while status != "Succeeded":
+                time.sleep(int(retry_after))
+                response = requests.get(operation_location, headers=headers)
+                status = response.json()['status']
+            image_url = response.json()['result']['contentUrl']
+            print(image_url)
+
             await message.channel.send(image_url)
 
 client.run(DISCORD_TOKEN)
